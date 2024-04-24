7015f8e16043e6a075af4471bfa461606af8e630
linlyboi <linlysmolworthy@gmail.com>
2024-04-24 19:50:06 +0200

    epic spelling fail

--- migrations/complaints.sql
+++ migrations/complaints.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS complaint
 (
 	id BIGSERIAL PRIMARY KEY,
 	title TEXT NOT NULL,
-	desciption TEXT NOT NULL,
+	description TEXT NOT NULL,
 	status NOT NULL DEFAULT open,
 	-- TODO decide if tags should be TEXT or an ENUM
 	tags TEXT,
