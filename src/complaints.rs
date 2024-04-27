bf1b34a0c40b80e654faf58a409a68f215310e71
aliashraf123 <97912096+aliasharf123@users.noreply.github.com>
2024-04-28 00:04:32 +0300

    Update complaints.rs

--- src/complaints.rs
+++ src/complaints.rs
@@ -25,13 +25,47 @@ struct Complaint {
 pub async fn get_complaints(state: Data<AppState>) -> impl Responder {
     let db_pool = &state.get_ref().db;
     let complaints = query_as!(
-        Complaint,
-        r#"SELECT id, title, description, status as "status!: Status", tags FROM complaint"#
-    )
+		Complaint,
+		r#"SELECT id, title, description, status as "status!: Status", tags FROM complaint WHERE status='open'"#
+	)
     .fetch_all(db_pool)
     .await
     .expect("Could not fetch complaints");
     HttpResponse::Ok()
         .content_type("application/json")
         .json(complaints)
+}
+
+#[get("closed")]
+pub async fn get_closed_complaints(state: Data<AppState>) -> impl Responder {
+    let db_pool = &state.get_ref().db;
+    let complaints = query_as!(
+		Complaint,
+		r#"SELECT id, title, description, status as "status!: Status", tags FROM complaint WHERE status='closed'"#
+	)
+    .fetch_all(db_pool)
+    .await
+    .expect("Could not fetch complaints");
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(complaints)
+}
+
+#[post("new")]
+async fn insert_complaint(
+    db_pool: Data<AppState>,
+    complaint: web::Query<Complaint>,
+) -> impl Responder {
+    let db_pool = &state.get_ref().db;
+    query!(
+        "INSERT INTO complaint (title,description,status,tags) VALUES ($1, $2, $3, $4)",
+        complaint.title,
+        complaint.description,
+        complaint.status,
+        complaint.tags
+    )
+    .execute(db_pool.get_ref())
+    .await
+    .expect("I shat");
+    HttpResponse::Ok().body("inserted")
 }
