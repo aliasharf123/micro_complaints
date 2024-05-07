0cb5b5dd82f9c610a2bb6ed9cc1b3c7e2796a967
linlyboi <linlysmolworthy@gmail.com>
2024-05-07 21:24:23 +0300

    split to tests.rs file

--- server/src/mail/mod.rs
+++ server/src/mail/mod.rs
@@ -1,11 +1,11 @@
-use std::{env, error::Error};
+#[cfg(test)]
+mod tests;
 
+use std::{env, error::Error};
 use actix_web::web::Json;
 use reqwest::{Client, Response};
 use serde::Serialize;
 
-// url /mail/send
-// body: {"mail_subject": "somevalue", ""}
 #[allow(non_snake_case)]
 #[derive(Serialize)]
 struct Mail {
@@ -27,20 +27,4 @@ pub async fn send_mail(
 	let root_url = env::var("MAIL_URL").expect("No MAIL_URL set in .env");
 	let client = Client::new();
 	client.post(root_url).json(&mail).send().await
-}
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[actix_web::test]
-	async fn send_mail_test() {
-		dotenvy::dotenv().ok();
-		let response = send_mail(
-			String::from("realest.roosya@gmail.com"),
-			String::from("LONELY NIGHTS"),
-			String::from("FADES INTO HIS WHITE NIKES"),
-		).await;
-		dbg!(&response);
-		assert!(response.is_ok())
-	}
 }
